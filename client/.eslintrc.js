c85eb5f03a49b0203983affb1b0c4eb6aa8a7ba4
Krzysztof Kalisz <kalisz.krzysztofl@gmail.com>
2020-12-30 17:31:56 +0100

    Add basic client structure

---
+++ client/.eslintrc.js
@@ -0,0 +1,86 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+    // 'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'max-len': [
+      'error',
+      {
+        code: 150,
+      },
+    ],
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'import/extensions': 0,
+    'operator-linebreak': 'off',
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        declaration: 'parens',
+        assignment: 'parens',
+        return: 'parens',
+        arrow: 'parens',
+        condition: 'ignore',
+        logical: 'ignore',
+        prop: 'ignore',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 120,
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['./src'],
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NextPage } from 'next';
+import TestComponent from 'components/TestComonent';
+
+const Home: NextPage = () => <TestComponent />;
+
+export default Home;
