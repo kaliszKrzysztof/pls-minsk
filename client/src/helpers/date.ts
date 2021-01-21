8357406e78ad00aa975019ce0be35ca251be2ee0
Krzysztof Kalisz <kalisz.krzysztofl@gmail.com>
2021-01-22 00:09:51 +0100

    Add timezone plugin to dayjs instance

--- client/src/helpers/date.ts
+++ client/src/helpers/date.ts
@@ -1,6 +1,11 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import 'dayjs/locale/pl';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 dayjs.locale('pl');
 dayjs.extend(relativeTime);
